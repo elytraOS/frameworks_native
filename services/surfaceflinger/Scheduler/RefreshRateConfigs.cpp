82875619ea49dfad84a78285a65ef81daf620120
Juhyung Park <qkrwngud825@gmail.com>
2022-05-22 04:37:03 +0900

    SF: Scheduler: opportunistically enter idle after touch
    This commit drastically changes how the scheduler works.
    
    Now, the device will be forced to maintain idle state (set to 60 Hz in
    this commit) after touchboost is finished and idle state is entered once.
    
    The main principle of this commit is that the highest refresh rate isn't
    necessary for everything and applying it only for touch-related events
    are plenty enough.
    
    It's been deliberately set to 60 Hz so that anomalies from the nature of
    imperfectiveness of this method is minimum and less-noticeable compared
    to the lowest possible refresh rate.
    
    Example of this commit in action:
    
    <Before 1>
    | Touch starts | Scrolling starts | Touch ends | Scrolling ends | Scroll-bar timeout | Scroll-bar fade-out animation | Idle  |
    | 120 Hz       | 120 Hz           | 120 Hz     | 120 Hz         | 24 Hz              | 120 Hz                        | 24 Hz |
    
    <After 1>
    | Touch starts | Scrolling starts | Touch ends | Scrolling ends | Scroll-bar timeout | Scroll-bar fade-out animation | Idle  |
    | 120 Hz       | 120 Hz           | 120 Hz     | 120 Hz         | 24 Hz              | 60 Hz                         | 24 Hz |
    
    <Before 2>
    | Idle  | Wi-Fi status bar icon changes | Timeout | Idle  |
    | 24 Hz | 120 Hz                        | 120 Hz  | 24 Hz |
    
    <After 2>
    | Idle  | Wi-Fi status bar icon changes | Timeout | Idle  |
    | 24 Hz | 60 Hz                         | 60 Hz   | 24 Hz |
    
    Change-Id: I27dc60f35821ee4a5726f67f9b538b394194d00c
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- services/surfaceflinger/Scheduler/RefreshRateConfigs.cpp
+++ services/surfaceflinger/Scheduler/RefreshRateConfigs.cpp
@@ -274,14 +274,17 @@ RefreshRate RefreshRateConfigs::getBestRefreshRateLocked(
         GlobalSignals* outSignalsConsidered) const {
     ATRACE_CALL();
     ALOGV("getBestRefreshRate %zu layers", layers.size());
+    static bool localIsIdle;
 
     if (outSignalsConsidered) *outSignalsConsidered = {};
     const auto setTouchConsidered = [&] {
         outSignalsConsidered->touch = true;
+        localIsIdle = false;
     };
 
     const auto setIdleConsidered = [&] {
         outSignalsConsidered->idle = true;
+        localIsIdle = true;
     };
 
     int noVoteLayers = 0;
@@ -444,6 +447,20 @@ RefreshRate RefreshRateConfigs::getBestRefreshRateLocked(
             ? getBestRefreshRate(scores.rbegin(), scores.rend())
             : getBestRefreshRate(scores.begin(), scores.end());
 
+    const auto selectivelyForceIdle = [&] {
+        ALOGV("localIsIdle: %s", localIsIdle ? "true" : "false");
+        if (localIsIdle && bestRefreshRate->getFps().greaterThanWithMargin(Fps(60.0f))) {
+            /*
+             * We heavily rely on touch to boost higher than 60 fps.
+             * Fallback to 60 fps if an higher fps was calculated.
+             */
+            ALOGV("Forcing idle");
+            return *idleRefreshRate;
+        }
+
+        return *bestRefreshRate;
+    };
+
     if (primaryRangeIsSingleRate) {
         // If we never scored any layers, then choose the rate from the primary
         // range instead of picking a random score from the app range.
@@ -453,7 +470,7 @@ RefreshRate RefreshRateConfigs::getBestRefreshRateLocked(
                   getMaxRefreshRateByPolicyLocked().getName().c_str());
             return getMaxRefreshRateByPolicyLocked();
         } else {
-            return *bestRefreshRate;
+            return selectivelyForceIdle();
         }
     }
 
@@ -479,7 +496,7 @@ RefreshRate RefreshRateConfigs::getBestRefreshRateLocked(
         return touchRefreshRate;
     }
 
-    return *bestRefreshRate;
+    return selectivelyForceIdle();
 }
 
 std::unordered_map<uid_t, std::vector<const RefreshRateConfigs::LayerRequirement*>>
@@ -837,6 +854,11 @@ void RefreshRateConfigs::getSortedRefreshRateListLocked(
     outRefreshRates->reserve(mRefreshRates.size());
     for (const auto& [type, refreshRate] : mRefreshRates) {
         if (shouldAddRefreshRate(*refreshRate)) {
+            if (refreshRate->getFps().equalsWithMargin(Fps(60.0f))) {
+                idleRefreshRate = std::make_unique<RefreshRateConfigs::RefreshRate>(*refreshRate);
+                ALOGV("idleRefreshRate set!");
+            }
+
             ALOGV("getSortedRefreshRateListLocked: mode %d added to list policy",
                   refreshRate->getModeId().value());
             outRefreshRates->push_back(refreshRate.get());
